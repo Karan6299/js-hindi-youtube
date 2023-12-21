4a8fe4d4c20432b25e901e94c16b8424acddd343
Karan6299 <129037173+Karan6299@users.noreply.github.com>
2023-12-21 05:02:55 +0000

    Data Types

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)// we are using node js not browser
+
+console.log(3 
+    + 3); // code readability should be help
+
+
+console.log("Karan kumar")
+
+let name = "karan"
+let age = 18
+let isLoggedIn = false
+let state = "null"
+
+//number => 2 to power
+//bigInt 
+//string => ""
+// Boolean => true/false
+// null => standalone value
+//undefind => 
+//symbol => unique
+
+//object 
+
+console.log(typeof null)
+console.log(typeof undefined)
